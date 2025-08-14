e3c74352d5caa6e2ab6aaa78bc4d59415049d999
shao <68085490+shaoming11@users.noreply.github.com>
2025-08-14 16:06:05 -0400

    query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,32 @@
 import { useQuery } from "@tanstack/react-query"
+import { useState } from "react"
 
 function App() {
+  const [id, setId] = useState(1);
+  const [on, setOn] = useState(true);
 
   const { data, isPending, refetch, error } = useQuery({
-    queryKey: ['todo'],
-    queryFn: getTodos,
+    queryKey: ['todo', id],
+    queryFn: () => getTodos(id),
+    enabled: on,
   });
 
   if (error) {
-    alert("Something went wrong")
+    alert("Some thing went wrong")
   }
 
   return (
     <>
       <div>{isPending ? <h1>LOADING </h1> : JSON.stringify(data.slice(0,10))}</div>
       <button onClick={() => refetch()}>refetch</button>
+      <button onClick={() => setId(i => i+1)}>increment id</button>
     </>
   )
 }
 
-async function getTodos() {
+async function getTodos(id: number) {
   await new Promise((resolve) => setTimeout(resolve, 1000))
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+  const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`)
   return await response.json();
 }
 
