740406bc2382868e541fbb273f98135eff9f599a
shao <68085490+shaoming11@users.noreply.github.com>
2025-09-09 22:47:23 -0400

    sep 9

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react'
 
 export default function Home() {
-  const [team, setTeam] = useState('1165A')
+  const [team, setTeam] = useState('1165')
 
   const addTeam = () => {
     
