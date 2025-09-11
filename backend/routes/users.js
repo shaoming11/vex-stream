c87439050851fe3fac507619969512c1f8da93ea
shao <68085490+shaoming11@users.noreply.github.com>
2025-09-10 23:15:17 -0400

    sep 10

---
+++ backend/routes/users.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    res.send("User List")
+})
+
+router.get("/new", (req, res) => {
+    res.send("User New Form")
+})
+
+router.post("/create", (req, res) => {
+    res.send("Create new user")
+})
+
+router
+    .route("/:id")
+    .get(
+    (req, res) => {
+        res.send(`Create new user with ID ${req.params.id}`)
+    })
+    .put(
+    (req, res) => {
+        res.send(`Update user with ID ${req.params.id}`)
+    })
+    .delete(
+    (req, res) => {
+        res.send(`Delete user with ID ${req.params.id}`)
+    })
+
+module.exports = router
