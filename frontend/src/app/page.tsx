c691ed9ca52f70cfcc00e3e657f59d61e5cf0075
shao <68085490+shaoming11@users.noreply.github.com>
2025-09-16 22:59:19 -0400

    sep 16

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,8 +5,9 @@ import { useState, useEffect } from 'react'
 export default function Home() {
   const [team, setTeam] = useState('1165')
 
-  const addTeam = () => {
-    
+  const getTeam = async () => {
+    const req = await fetch('localhost://5001/users/')
+    console.log(req.json())
   }
 
   useEffect(() => {
@@ -18,9 +19,9 @@ export default function Home() {
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1>Vex Stream</h1>
+      <h1 id="section-1"></h1>
       <input type="text" id="teamNumber" placeholder="Search for a team number" className="border-2"/>
-      <button onClick={addTeam}>Add Team</button>
+      <button onClick={getTeam}>get Team</button>
       <button>Load Team Info</button>
       <button>Delete Team</button>
       <input type="text" id="description" placeholder="Enter team description" className="border-2"/>
