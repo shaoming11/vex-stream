02db50922f32b2983376f82c3b988a14efd00b32
shao <68085490+shaoming11@users.noreply.github.com>
2025-10-25 21:48:59 -0400

    oct 25

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
+const VEX_KEY = process.env.VEX_KEY
+
 router.get("/", (req, res) => {
     res.send("user list")
 })
 
+router.get("/search", async (req, res) => {
+    const response = await fetch('https://www.robotevents.com/api/v2/teams/', {
+        headers: {
+            'Authorization': `Bearer ${VEX_KEY}`,
+            'Content-Type': 'application/json'
+        }
+    })
+    console.log(response)
+    const data = await response.json()
+    console.log("helloooo")
+    res.send("gurt")
+})
+
 router.get("/new", (req, res) => {
-    res.send("User New Form")
+    res.send("User New Form!")
 })
 
 router.post("/create", (req, res) => {
